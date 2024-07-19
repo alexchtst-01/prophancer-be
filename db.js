8beb0f2ff490de361ea8f6acad9cba94671fcd03
alexchtst-01 <acinatra@gmail.com>
2024-07-19 13:12:42 +0700

    commit

---
+++ db.js
@@ -0,0 +1,22 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const db = new Sequelize(
+  process.env.PGDATABASE,
+  process.env.PGUSER,
+  process.env.PGPASSWORD,
+  {
+    host: process.env.PGHOST,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
+);
+
+export default db;
